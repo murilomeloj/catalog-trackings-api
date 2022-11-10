05f6cce71cab75fda82420e589dcd1875ff5d9e0
murilomeloj <86209786+murilomeloj@users.noreply.github.com>
2022-11-10 14:37:34 -0300

    some minor fixes

--- index.js
+++ index.js
@@ -17,10 +17,21 @@ const api = axios.create({
 api.defaults.headers.common['Authorization'] = process.env.SECRET_API;
 
 app.post("/registerCart", async (req, res) => {
+    const pratos = ["Prato 01","Prato 02","Prato 03"];
+    const bebidas = ["Bebida 01", "Bebida 02", "Bebida 03"];
+    const sobremesas = ["Sobremesa 01", "Sobremesa 02", "Sobremesa 03"];
+    let category = "";
     const { items } = req.body;
     const products = items.split(',');
-    console.log(products.length);
   for(let item of products) {
+        if(pratos.includes(item.trim()))
+            category = "Pratos mais escolhidos";
+        else if(bebidas.includes(item.trim())){
+            category = "Bebidas mais escolhidas";
+        }
+        else if(sobremesas.includes(item.trim())){
+            category = "Sobremesas mais escolhidas";
+        }
         let { data } = await api.post('https://http.msging.net/commands', {
             "id": Math.floor(Date.now() * Math.random()).toString(36),
             "to": "postmaster@analytics.msging.net",
@@ -28,7 +39,7 @@ app.post("/registerCart", async (req, res) => {
             "type": "application/vnd.iris.eventTrack+json",
             "uri": "/event-track",
             "resource": {
-                "category": "Pratos mais escolhidos",
+                "category": category,
                 "action": item.trim()
             }
         });
